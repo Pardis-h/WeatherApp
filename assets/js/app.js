a1bce9fbfaa8498e92ad362f5af82bcccc5f401b
Pardis Haghdoust <pardis.haghdoust@gmail.com>
2022-06-24 20:10:13 +0430

    Toggle between Fahernheit and celsius Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,13 +13,18 @@ closeBtn.addEventListener("click",() => {
     searchBox.classList.add("hidden");
 })
 
-// get Weather with an API
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}&unit=metric
+
+// SELECT ELEMENTS
 const form = document.querySelector(".search-form");
 const input = document.querySelector(".search-form input");
 const msg = document.querySelector(".msg");
 const ulList= document.querySelector(".day5forecat");
+const historyUlList = document.querySelector(".search-history");
+
 const apiKey = "f244a8c180930c1583ec919bc958b7b6";
+
+const unitsF =document.querySelector(".fahrenhiet");
+const unitsC =document.querySelector(".celsius");
 
 const todayTemp = document.querySelector(".degree");
 const weatherImgSrc = document.getElementById("weather-img");
@@ -31,29 +36,100 @@ const humidityRange = document.querySelector(".humidity-range");
 const weatherVisibility = document.querySelector(".weather-visibility");
 const pressure = document.querySelector(".pressure");
 const fellLikes = document.querySelector(".feel-likes");
+// const unitImageSrc = document.querySelector(".units-img").src;
+// console.log(unitImageSrc);
 
+// Get Today Date
 const date = new Date();
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const mounts = ['January', 'February', 'March', 'April', 'May', 'June', 'July' , 'August' , 'September' , 'October' , 'November' , 'December'];
+
 todayDate.innerText = `${days[date.getDay()]} ${date.getDate()} ${mounts[date.getMonth()]}`;
 
-form.addEventListener("submit" , e => {
+// Toggle Fahrenheit and Celisius
+unitsF.addEventListener("click",e =>{
+    searchActiveUnitsF();
+    if(unitsF.classList.contains("units-active")){
+        linkUrlF = historyUlList.lastElementChild.querySelector(".search-item").dataset.name;
+        getWeather(linkUrlF);
+        document.querySelector(".current-weather .units-img").src = "./assets/img/fahrenhiet.png";
+        document.querySelector(".current-feel .units-img").src = "./assets/img/fahrenhiet.png";   
+    }
+});
+function searchActiveUnitsF(){
+    if(!(unitsF.classList.contains("units-active"))){
+        unitsF.classList = "fahrenhiet bg-white text-slate-500 hover:cursor-pointer rounded-full py-2 px-3 units-active";
+        unitsC.classList = "celsius bg-slate-600/[.6] hover:bg-white hover:text-slate-500 hover:cursor-pointer transition duration-300 ease-in-out rounded-full py-2 px-3 mx-3";
+    }
+}
+unitsC.addEventListener("click",e=>{
+    searchActiveUnitsC();
+    if(unitsC.classList.contains("units-active")){
+        linkUrlC = historyUlList.lastElementChild.querySelector(".search-item").dataset.name;
+        getWeather(linkUrlC);
+        document.querySelector(".current-weather .units-img").src = "./assets/img/celsius.png";
+        document.querySelector(".current-feel .units-img").src = "./assets/img/celsius.png";       }
+});
+function searchActiveUnitsC(){
+    if(!(unitsC.classList.contains("units-active"))){
+        unitsC.classList = "celsius bg-white text-slate-500 hover:cursor-pointer rounded-full py-2 px-3 units-active";
+        unitsF.classList = "fahrenhiet bg-slate-600/[.6] hover:bg-white hover:text-slate-500 hover:cursor-pointer transition duration-300 ease-in-out rounded-full py-2 px-3 mx-3";
+    }
+}
+
+// Get Weather from search
+form.addEventListener("submit" , e =>{
     e.preventDefault();
-    let inputVal = input.value;
+
+    getWeather(input.value);
+
+    // search history List
+    const newSearch = document.createElement("li");
+    const newSearchMark = `
+    <a href="#" data-name="${input.value}" class="search-item flex justify-between border-solid border border-transparent hover:border-slate-600 transition duration-300 ease-in-out rounded px-4 py-2 mb-2">
+        <span class="leading-loose">${input.value}</span>
+        <i class="fa-solid fa-angle-right text-slate-500 leading-loose"></i>
+    </a>
+    `;
+    newSearch.innerHTML = newSearchMark;
+    historyUlList.appendChild(newSearch);
+    input.value="";
+});
+
+//Get search from Search History
+const searchLink = document.querySelectorAll(".search-item");
+searchLink.forEach(item =>{
+    item.addEventListener("click",e=>{
+        e.preventDefault();
+        let linkUrl = item.dataset.name;
+        console.log(linkUrl);
+        getWeather(linkUrl);
+    });
+});
+
+// Get weather API
+function getWeather(city){
+    const inputVal = city;
     const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${inputVal}&limit=5&appid=${apiKey}`;
     fetch(geoUrl)
         .then(responseGeo => responseGeo.json())
         .then(geoData => {
             const {lat ,lon} = geoData[0];
-            // const units = "";
-            const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+            let units = "metric";
+            unitImageSrc = "./assets/img/celsius.png";
+
+            if (unitsF.classList.contains("units-active")) {
+                units = "imperial";
+                unitImageSrc = "./assets/img/fahrenhiet.png";
+            }
+            const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
             fetch(url)
                 .then(response => response.json())
                 .then(data =>{
                     const {list,city:{name}}=data;
                     const {main,weather} = list[0];
                     const icon = weather[0]["icon"];
-                    const timesToDisplay = [0, 8, 16];
+                    const timesToDisplay = [0 , 8, 16 , 24 ];
                     console.log(list);
 
                     todayTemp.innerText = Math.round(main.temp);
@@ -81,45 +157,42 @@ form.addEventListener("submit" , e => {
         
                         if(dayName === days[date.getDay()]){
                             const listItems = document.createElement("li");
+                            listItems.classList="bg-slate-800 basis-6/12 max-w-[46%] md:max-w-none md:basis-auto rounded px-7 py-4";
                             const markup = `
-                                <div class="bg-slate-800 basis-6/12 max-w-[46%] md:max-w-none md:basis-auto rounded px-7 py-4" >
-                                    <p class="date text-slate-300 flex justify-between"><span>Today</span><span>${dayTime}</span></p>
-                                    <div class=" text-center mt-4 mb-5"><img src="./assets/img/${icons}.png" alt="${descs}"></div>
+                                    <p class="date text-slate-300 flex justify-between"><span>Today</span><span> ${dayTime}</span></p>
+                                    <div class=" text-center mt-4 mb-5"><img class="ml-auto mr-auto" src="./assets/img/${icons}.png" alt="${descs}"></div>
                                     <div class="flex justify-between">
-                                        <span class="highest ">${tempMax}<img src="./assets/img/celsius.png" class="w-4 inline-block" alt="Celsius" /></span>
-                                        <span class="loweset text-slate-500">${tempMin}<img src="./assets/img/celsius.png" class="w-4 inline-block" alt="Celsius" /></span>
+                                        <span class="highest ">${tempMax}<img src="${unitImageSrc}" class="units-img w-4 inline-block" alt="Units" /></span>
+                                        <span class="loweset text-slate-500">${tempMin}<img src="${unitImageSrc}" class=" units-img w-4 inline-block" alt="Units" /></span>
                                     </div>
-                                </div>
                             `;
                             listItems.innerHTML = markup;
                             ulList.appendChild(listItems);
                         }else if (timesToDisplay.includes(index) && dayName === days[date.getDay() + 1]) {
 
                             const listItems = document.createElement("li");
+                            listItems.classList="bg-slate-800 basis-6/12 max-w-[46%] md:max-w-none md:basis-auto rounded px-7 py-4";
                             const markup = `
-                                <div class="bg-slate-800 basis-6/12 max-w-[46%] md:max-w-none md:basis-auto rounded px-7 py-4" >
                                     <p class="date text-slate-300">Tommorow</p>
-                                    <div class=" text-center mt-4 mb-5"><img src="./assets/img/${icons}.png" alt="${descs}"></div>
+                                    <div class=" text-center mt-4 mb-5"><img class="ml-auto mr-auto" src="./assets/img/${icons}.png" alt="${descs}"></div>
                                     <div class="flex justify-between">
-                                        <span class="highest ">${tempMax}<img src="./assets/img/celsius.png" class="w-4 inline-block" alt="Celsius" /></span>
-                                        <span class="loweset text-slate-500">${tempMin}<img src="./assets/img/celsius.png" class="w-4 inline-block" alt="Celsius" /></span>
+                                        <span class="highest ">${tempMax}<img src="${unitImageSrc}" class="units-img w-4 inline-block" alt="Units" /></span>
+                                        <span class="loweset text-slate-500">${tempMin}<img src="${unitImageSrc}" class="units-img w-4 inline-block" alt="Units" /></span>
                                     </div>
-                                </div>
                             `;
                             listItems.innerHTML = markup;
                             ulList.appendChild(listItems);
                         }else if (timesToDisplay.includes(index) && dayName !== days[date.getDay() + 1]) {
 
                             const listItems = document.createElement("li");
+                            listItems.classList="bg-slate-800 basis-6/12 max-w-[46%] md:max-w-none md:basis-auto rounded px-7 py-4";
                             const markup = `
-                                <div class="bg-slate-800 basis-6/12 max-w-[46%] md:max-w-none md:basis-auto rounded px-7 py-4" >
                                     <p class="date text-slate-300">${dayName} , ${d.getDate()} ${mounts[d.getMonth()]}</p>
-                                    <div class=" text-center mt-4 mb-5"><img src="./assets/img/${icons}.png" alt="${descs}"></div>
+                                    <div class=" text-center mt-4 mb-5"><img class="ml-auto mr-auto" src="./assets/img/${icons}.png" alt="${descs}"></div>
                                     <div class="flex justify-between">
-                                        <span class="highest ">${tempMax}<img src="./assets/img/celsius.png" class="w-4 inline-block" alt="Celsius" /></span>
-                                        <span class="loweset text-slate-500">${tempMin}<img src="./assets/img/celsius.png" class="w-4 inline-block" alt="Celsius" /></span>
+                                        <span class="highest ">${tempMax}<img src="${unitImageSrc}" class="units-img w-4 inline-block" alt="Units" /></span>
+                                        <span class="loweset text-slate-500">${tempMin}<img src="${unitImageSrc}" class=" units-img w-4 inline-block" alt="Units" /></span>
                                     </div>
-                                </div>
                             `;
                             listItems.innerHTML = markup;
                             ulList.appendChild(listItems);
@@ -132,5 +205,8 @@ form.addEventListener("submit" , e => {
             msg.innerText="Please search a valid city";
         })
     ulList.innerHTML="";
-    input.value="";
-})
+}
+
+getWeather("Rasht");
+
+
